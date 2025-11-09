f434d233665ebfd7f2858ecff676453f140f4f72
Janmikaj <jayamj.bsc23@rvu.edu.in>
2025-11-09 12:28:48 +0530

    Stable working version

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000", // Your backend
+  baseURL: "http://localhost:5001", // Your backend
 });
 
 export default api;
